0d1586f75f28a775ca5a4afc9be2956c3c33a420
DanViTrinh <danvitrinh@outlook.com>
2024-04-23 11:36:39 +0200

    removed battery notification when full

--- .config/hypr/scripts/batterynotify.sh
+++ .config/hypr/scripts/batterynotify.sh
@@ -25,9 +25,9 @@ if is_laptop; then
 			dunstify -u CRITICAL "Battery Low" "Battery is at $battery_percentage%. Connect the charger."
 		fi
 
-		if [ "$battery_status" == "Charging" ] && [ "$battery_percentage" -ge 80 ]; then
-			dunstify -u NORMAL "Battery Charged" "Battery is at $battery_percentage%. You can unplug the charger."
-		fi
+		# if [ "$battery_status" == "Charging" ] && [ "$battery_percentage" -ge 80 ]; then
+		# 	dunstify -u NORMAL "Battery Charged" "Battery is at $battery_percentage%. You can unplug the charger."
+		# fi
 
 		sleep 300 # Sleep for 5 minutes before checking again
 	done
