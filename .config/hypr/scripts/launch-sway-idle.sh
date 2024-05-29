f11a82274b3c84beea8c7ba5d2d0d3a451628c0f
DanViTrinh <danvitrinh@outlook.com>
2024-05-29 17:53:07 +0200

    switch order of autostart

--- .config/hypr/scripts/launch-sway-idle.sh
+++ .config/hypr/scripts/launch-sway-idle.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
+sway-audio-idle-inhibit &
+
 swayidle -w timeout 300 'swaylock' \
 	timeout 360 'hyprctl dispatch dpms off' \
 	resume 'hyprctl dispatch dpms on' \
 	timeout 600 'systemctl suspend' &
-
-sway-audio-idle-inhibit &
